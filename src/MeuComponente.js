22a4854c8c17c413f17e981e6aa4398c83032e99
Robert <robertmaio@outlook.com>
2024-08-02 09:58:12 -0300

    feat: new elements

---
+++ src/MeuComponente.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MeuComponente() {
+  return (
+    <div>
+      <h2>Este é um Componente Adicional</h2>
+      <p>Componente criado para demonstrar a reutilização em React.</p>
+    </div>
+  );
+}
+
+export default MeuComponente;
